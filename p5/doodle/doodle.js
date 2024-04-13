24d1fe9e908a4f9c5277528cdca51a4292bf225b
karen ann <karenanndonnachie@gmail.com>
2024-04-13 21:08:07 +1000

    Update doodle.js

--- p5/doodle/doodle.js
+++ p5/doodle/doodle.js
@@ -2,6 +2,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   stroke(255);
   background(0);
+  p=createP("'S' to save, 'SPACE' to clear");
+  p.position(20,20);
+  p.style('color','white');;
 }
 
 function draw() {
