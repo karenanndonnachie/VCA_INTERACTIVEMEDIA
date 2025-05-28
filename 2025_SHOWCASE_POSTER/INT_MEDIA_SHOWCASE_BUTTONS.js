2f096c95e51d2a2b7cf14da2ecc245a48db6dcb6
karen ann <karenanndonnachie@gmail.com>
2025-05-28 15:32:31 +1000

    Update INT_MEDIA_SHOWCASE_BUTTONS.js

--- 2025_SHOWCASE_POSTER/INT_MEDIA_SHOWCASE_BUTTONS.js
+++ 2025_SHOWCASE_POSTER/INT_MEDIA_SHOWCASE_BUTTONS.js
@@ -1,15 +1,11 @@
 function setup() {
 createCanvas(windowWidth, windowHeight);
 background("blue");
-let myButton=createButton("INTERACTIVE MEDIA");
+let myButton=createButton("CLICK ME");
 myButton.position(100, 100);
 rectMode(CENTER);
 textAlign(CENTER, CENTER);
 myButton.mousePressed(spawn);
-//let div=createDiv("I AM A DIV!");
-//div.position(width/2, height/2);
-//let link = createA("https://becausewhy.org", "linkylink");
-//link.position(200, 600);
 }
 function draw() {
 
